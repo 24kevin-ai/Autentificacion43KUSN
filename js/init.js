bc787a32d0fea2cacf7cc6cb298e0327da6ee33a
24kevin-ai <81113762+24kevin-ai@users.noreply.github.com>
2021-03-23 08:17:58 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/* Initializa Firebase con la configuración del proyecto. Revisar la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignorar
+base de incendios. initializeApp({
+    apiKey: "AIzaSyDNB5IdpaOiw7iGSfNJrk6AR8bWbiQlxUU",
+    authDomain: "autentificacion43gvlf.firebaseapp.com",
+    projectId: "autentificacion43gvlf",
+    storageBucket: "autentificacion43gvlf.appspot.com",
+    messagingSenderId: "1012902297327",
+    appId: "1:1012902297327:web:e43b8b093fef7c813ca51a",
+    measurementId: "G-PFWFFP9EPJ"
+});
